dc7e1892fe848678deaa7c4bd398cdc4b17c6e77
Andrew Nguyen <nguyendrew20@gmail.com>
2023-04-25 14:31:18 -0700

    new website title message

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -5,6 +5,7 @@ import Directory from '../../components/directory/directory.component';
 const Home = () => {
   return (
     <div>
+       <h1 style={{ textAlign: 'center' }}>Welcome to Monarch Clothing</h1>
       <Directory />
       <Outlet />
     </div>
